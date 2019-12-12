9d66eba33582c005cf0a3a25007cbae41c44b5ca
Tammy Lam <lamtammy1515@gmail.com>
2019-12-12 17:17:07 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,23 @@
 require 'pry'
 
 def my_all?(collection)
+ index = 0
 
+while index < collection.length
+      if yield(collection[index]) == false
+           return false 
+    end 
+
+index += 1 
 end
+end
+
+collection = [1, 2, 3]
+
+my_all?(collection) do |i|
+  i < 3
+end
+
+my_all?(collection) do |i| 
+  i < 4 
+end 
